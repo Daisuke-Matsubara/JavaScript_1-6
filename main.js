e93be8a0b62cd3f5e3c3657801551b88d9eef79c
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2023-02-11 08:41:11 +0900

    JavaScript初級 第1章配列 課題

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-let scores = [50,60,70,80,90,'百'];
-for (let i = 0; i < scores.length; i++) {
-    console.log(`得点は${scores[i]}点です`)
+//課題 問１
+let scores = [10,15,20,25];
+console.log(scores);
+for (let i = 0; i < scores.length;i++ ) {
+    if(scores[i]%2 ===0){
+        console.log(`${scores[i]}は偶数です`);}
+}
+
+//課題 問２
+let car = { gass:20.5 , num:1234 };
+console.log(car);
+console.log(`ガソリンは${car.gass}です`);
+console.log(`ナンバーは${car.num}です`);
+
+//練習
+let scores1 = [50,60,70,80,90,'百'];
+for (let i = 0; i < scores1.length; i++) {
+    console.log(`得点は${scores1[i]}点です`)
 }
 
 let human = {
