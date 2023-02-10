56de1912e24388b5cabe9a3818cd3c6f0a35dc60
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2023-02-10 12:09:47 +0900

    R5.2.10

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-let scores = [50,60,70,80,90];
-for (let i = 0; i < 5; i++) {
+let scores = [50,60,70,80,90,'百'];
+for (let i = 0; i < scores.length; i++) {
     console.log(`得点は${scores[i]}点です`)
 }
+
+let human = {
+    name:'yamada',
+    height:170,
+    weight:60,
+    gender:"男",
+    age:30
+};
+
+console.log(human.name);
+console.log(human.height);
+console.log(`年齢は${human.age}歳です`);
